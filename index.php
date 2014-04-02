659513521dc1ce5f35e8e219ceec4d16fc6f6811
roshenw <rw2485@columbia.edu>
2014-04-02 13:26:00 -0400

    installed bootstrap template

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
+<!DOCTYPE html>
 <html>
-<head>
-
-	<meta charset="utf-8"/>
-	<title>Viducate</title>
-</head>
-<body>
-	<p>Education Should be Free</p>
-</body>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
 </html>
