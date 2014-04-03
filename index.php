dbb8319612fa09cc3c2c92845a63d92eb209e7b7
roshenw <rw2485@columbia.edu>
2014-04-03 12:03:44 -0400

    Home page centered

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
     <!-- <div class="col-lg-5 col-lg-offset-4  putColor"> -->
     <div class="main-container">
     <div class="putColor">
-    <h1>Viducate!</h1>
+    <h1 class="font-color">Viducate!</h1>
     <div class="">
  	  	<div class="navbar">
-    		<ul class="nav navbar-nav">
-    			<li><a href="index.php">Home</a></li>
+    		<ul class="nav navbar-nav navbar-position">
+    			<li><a href="index.php" class="font-color">Home</a></li>
     			<li class= "dropdown">
-    				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Departments <span class="caret"></span></a>
-    				<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+    				<a href="#" class="dropdown-toggle font-color" data-toggle="dropdown">Departments <span class="caret"></span></a>
+    				<ul class="dropdown-menu font-color" role="menu" aria-labelledby="dropdownMenu">
     					<li><a tabindex="-1" href="#">Art</a></li>
   						<li><a tabindex="-1" href="#">Computer Science</a></li>
   						<li><a tabindex="-1" href="#">Economics</a></li>
@@ -32,10 +32,10 @@
 		</div>
   </div>  
 
-    <div class="col-xs-5 col-sm-10" sytle="background-color:red ;">
+    <div class="col-xs-5 col-sm-10 " sytle="background-color:red ;">
         <form class="navbar-form" role="search">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+          <div class="input-group searchbar-position">
+            <input type="text" class="form-control" placeholder="Search by subject, topic" name="srch-term" id="srch-term">
             <div class="input-group-btn">
               <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
             </div>
