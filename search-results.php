2196531cef735f2f6f12183c075e06470c5fa9c8
roshenw <rw2485@columbia.edu>
2014-04-03 16:02:44 -0400

    Search-results page done - only need to implement the php in there

--- search-results.php
+++ search-results.php
@@ -1,3 +1,7 @@
+<?php 
+	$video_link = "R0EQg9vgbQw";
+ ?>
+
 <html>
 <head>
 	<title>Viducate</title>
@@ -8,31 +12,79 @@
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
 </head>
 <body id="home" class="main-container">
-<table class="table table-hover" style="color:white">
-  <tr>
-  <td><iframe class="vid-container" src="http://www.youtube.com/embed/R0EQg9vgbQw"></iframe></td>
-  <td>126</td>
-  </tr>
-  <tr>
-  <td><iframe class="vid-container" src="http://www.youtube.com/embed/R0EQg9vgbQw"></iframe></td>
-  <td>13456</td>  
-  </tr>
-  <tr>
-  <td><iframe class="vid-container" src="http://www.youtube.com/embed/R0EQg9vgbQw"></iframe></td>
-  <td>423426</td>
-  </tr>
-  <tr>
-  <td><iframe class="vid-container" src="http://www.youtube.com/embed/R0EQg9vgbQw"></iframe></td>
-  <td>23443</td>
-  </tr>
-  <tr>
-  <td><iframe class="vid-container" src="http://www.youtube.com/embed/R0EQg9vgbQw"></iframe></td>
-  <td>875126</td>
-  </tr>	
-  
-  
-  	
-</table>
 
+
+<div class="">
+    <div class="">
+    <h1 class="font-color">Viducate</h1>
+    <div class="">
+ 	  	<div class="navbar">
+    		<ul class="nav navbar-nav ">
+    			<li><a href="index.php" class="font-color">Home</a></li>
+    			<li class= "dropdown">
+    				<a href="#" class="dropdown-toggle font-color" data-toggle="dropdown">Departments <span class="caret"></span></a>
+    				<ul class="dropdown-menu font-color" role="menu" aria-labelledby="dropdownMenu">
+    					<li><a tabindex="-1" href="#">Art</a></li>
+  						<li><a tabindex="-1" href="#">Computer Science</a></li>
+  						<li><a tabindex="-1" href="#">Economics</a></li>
+						<li><a tabindex="-1" href="#">Math</a></li>
+						<li><a tabindex="-1" href="#">Physics</a></li>			
+    				</ul>
+    			</li>
+    			<li>
+    				<div class="col-xs-5 col-sm-10 " sytle="background-color:red ;">
+        <form class="navbar-form" role="search">
+          <div class="input-group searchbar-position">
+            <input type="text" class="form-control" placeholder="Search by subject, topic" name="srch-term" id="srch-term">
+            <div class="input-group-btn">
+              <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+            </div>
+          </div>
+        </form>
+     </div>
+    			</li>
+    		</ul>
+		</div>
+  </div>  
+
+    
+
+
+
+
+<div style ="padding:5%;">
+
+
+<table class="table table-hover" style="color:white">
+	<h1  style="color:white;">Searched for  "Search String"</h1>
+	<p  style="color:white; margin-left:90%; content-align:right;">Views</p>
+  <tr>
+  <td><iframe class="vid-container" src="http://www.youtube.com/embed/<?php echo $video_link ?>"></iframe></td>
+  <td><button type="button" class="btn btn-primary">Like</button><button type="button" class="btn btn-danger">Dislike</button></td> 
+  <td>13226</td>
+  </tr>
+   <tr>
+  <td><iframe class="vid-container" src="http://www.youtube.com/embed/<?php echo $video_link ?>"></iframe></td>
+  <td><button type="button" class="btn btn-primary">Like</button><button type="button" class="btn btn-danger">Dislike</button></td> 
+  <td>54126</td>
+  </tr>
+   <tr>
+  <td><iframe class="vid-container" src="http://www.youtube.com/embed/<?php echo $video_link ?>"></iframe></td>
+  <td><button type="button" class="btn btn-primary">Like</button><button type="button" class="btn btn-danger">Dislike</button></td> 
+  <td>1526</td>
+  </tr>
+    <tr>
+  <td><iframe class="vid-container" src="http://www.youtube.com/embed/<?php echo $video_link ?>"></iframe></td>
+  <td><button type="button" class="btn btn-primary">Like</button><button type="button" class="btn btn-danger">Dislike</button></td> 
+  <td>95126</td>
+  </tr>
+   <tr>
+  <td><iframe class="vid-container" src="http://www.youtube.com/embed/<?php echo $video_link ?>"></iframe></td>
+  <td><button type="button" class="btn btn-primary">Like</button><button type="button" class="btn btn-danger">Dislike</button></td> 
+  <td>56126</td>
+  </tr>
+</table>
+<!-- end of table	 -->
+</div>
 </body>
 </html>
