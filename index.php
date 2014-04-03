e01e3a79ea0169bdc698a827405c5f82c2c27d6d
roshenw <rw2485@columbia.edu>
2014-04-02 20:28:47 -0400

    Search bar and nav bar added

--- index.php
+++ index.php
@@ -1,13 +1,47 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>Viducate</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='http://fonts.googleapis.com/css?family=Julius+Sans+One' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
   </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <body  id="home">
+    
+
+    <h1>Viducate!</h1>
+    <div class="container  col-lg-3 col-lg-offset-5 row-lg-10">
+    <div class="">
+ 	  	<div class="navbar">
+    		<ul class="nav navbar-nav">
+    			<li><a href="index.php">Home</a></li>
+    			<li class= "dropdown">
+    				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Departments <span class="caret"></span></a>
+    				<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+    					<li><a tabindex="-1" href="#">Art</a></li>
+  						<li><a tabindex="-1" href="#">Computer Science</a></li>
+  						<li><a tabindex="-1" href="#">Economics</a></li>
+						<li><a tabindex="-1" href="#">Math</a></li>
+						<li><a tabindex="-1" href="#">Physics</a></li>			
+    				</ul>
+    			</li>
+    		</ul>
+    		<div class="col-xs-5 col-sm-10	">
+        <form class="navbar-form" role="search">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+            <div class="input-group-btn">
+              <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+            </div>
+          </div>
+        </form>
+     </div>
+		</div>
+
+    	</div><!-- closing main navigation  -->
+  
+
     <script src="_/js/bootstrap.js"></script>
     <script src="_/js/myscript.js"></script>
   </body>
